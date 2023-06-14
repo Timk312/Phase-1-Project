374a212673d89ee432fa083ca0a17c250c84b94d
tim kingston <timkingston312@gmail.com>
2023-06-14 17:07:08 -0400

    BMI formula

--- selfproject/index.js
+++ selfproject/index.js
@@ -7,15 +7,20 @@ let heightOutput=document.getElementById('heightOutput')
 let weightOutputo=document.getElementById('weightOutput')
 let myBMI=document.getElementById('bmiOutput')
 let bmi=0
-function submitInfo(){
 
+//updates the value of height and weight inputs into html
+//converts height and weight into integers
+//BMI=weight (lb) / [height (in)]2 x 703
+//updates BMI
+function submitInfo(){
     heightOutput.innerHTML=myHeight.value 
     weightOutput.innerHTML= myWeight.value
     myHeight=parseInt(heightOutput.innerHTML)
     myWeight=parseInt(weightOutput.innerHTML)
-    bmi=myWeight+myHeight
-    myBMI.innerHTML=bmi
-    
+    let inches=myHeight*12
+    bmi=(myWeight/(Math.pow(inches,2)))*703
+    myBMI.innerHTML=bmi  
 }
+//calls submitInfo() once button is clicked
 submitInfobtn.addEventListener("click",submitInfo)
 
